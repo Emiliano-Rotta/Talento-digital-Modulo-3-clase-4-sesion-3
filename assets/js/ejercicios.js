09871c0d7fddf8931b7f29ab6a041aaa600d91a5
Emiliano Rotta <emilianorottaf@gmail.com>
2024-08-29 22:06:32 -0300

    ejercicios para hacer

---
+++ assets/js/ejercicios.js
@@ -0,0 +1,55 @@
+//Ejercicio 1:
+// Escribe un programa que tome dos números y use if y else para imprimir cuál es mayor, o si son iguales.
+
+
+// let numUno = 2200
+// let numDos = 2200
+
+//  if(numUno < numDos) {
+//     console.log (`${numDos} es mayor que ${numUno}`)
+//  } else if (numUno > numDos){
+//     console.log (`${numUno} es mayor que ${numDos}`)
+//  } else {
+//     console.log (`${numDos} y ${numUno} son iguales`)
+//  }
+
+ // Ejercicio 2: Verificación de Edad
+// Escribe un programa que pregunte la edad de una persona y use if, else if, y else para determinar si la persona es un niño (menor de 12 años), un adolescente (entre 12 y 18 años), o un adulto (mayor de 18 años).
+
+
+// let edad = 19
+
+//  if(edad < 12) {
+//     console.log (`con ${edad} años: sos un niño`)
+//  } else if (edad >= 12 && edad <=18){
+//     console.log (`con ${edad} años: sos un adolescente`)
+//  } else {
+//     console.log (`con ${edad} años: sos un adulto`)
+//  }
+
+ // Ejercicio 3: Calificación de Notas
+// Crea un programa que tome una nota (entre 0 y 100) y use if, else if, y else para asignar una calificación (A, B, C, D, F).
+
+// Ejercicio 4: Número Par o Impar
+// Escribe un programa que determine si un número dado es par o impar usando el operador de igualdad ==.
+
+// Ejercicio 5: Rango de Números
+// Escribe un programa que tome un número y use && para verificar si está dentro del rango de 10 a 20 inclusive.
+
+// Ejercicio 6: Validación de Login
+// Crea un programa que verifique si un nombre de usuario y una contraseña coinciden con valores predefinidos usando && para comparar ambas condiciones.
+
+// Ejercicio 7: Clima y Actividad
+// Escribe un programa que sugiera una actividad (como "ir a la playa", "ver una película") basada en las condiciones climáticas. Usa || para permitir múltiples condiciones.
+
+// Ejercicio 8: Evaluación de Calificaciones
+// Crea un programa que tome dos calificaciones y determine si una persona ha aprobado el curso. Usa || para aprobar si una de las dos calificaciones es suficiente.
+
+// Ejercicio 9: Comparación de Tres Números
+// Escribe un programa que tome tres números y determine cuál es el mayor usando if, else if, y operadores de comparación.
+
+// Ejercicio 10: Acceso a Descuentos
+// Escribe un programa que determine si una persona es elegible para un descuento basado en su edad o si tiene una membresía. Usa || para evaluar las dos condiciones.
+
+
+//Ejercicio 6 8 y 9  ---> Tiempo hasta las 21.30 
